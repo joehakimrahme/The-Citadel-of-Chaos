08360552198d544728e744399c3a4da5b78a3e17
Joe H. Rahme <joehakimrahme@gmail.com>
2019-01-10 09:11:19 +0100

    Adds Hero attributes
    skill, stamina and luck are determined randomly at start (with dice
    rolls). They will vary during the course of a game but they will never
    go above their initial value or below zero. So we use `@property` to
    control this behavior.

--- tcoc/hero.py
+++ tcoc/hero.py
@@ -7,3 +7,86 @@ class Hero(object):
     @staticmethod
     def dice_roll(dice_count=1):
         return sum(random.randint(1, 6) for _ in range(dice_count))
+
+    def __init__(self, skill=None, stamina=None, luck=None, magic=None):
+        # provide random defaults if no values are provided
+        self._attributes = {
+            "skill": skill,
+            "stamina": stamina,
+            "luck": luck,
+            "magic": magic
+        }
+        # explicitly initalize properties to make sure we get valid
+        # values
+        if self._attributes["skill"] is None:
+            self._attributes["skill"] = self.dice_roll(1) + 6
+            self.initial_skill = self._attributes["skill"]
+        if self._attributes["stamina"] is None:
+            self._attributes["stamina"] = self.dice_roll(2) + 12
+            self.initial_stamina = self._attributes["stamina"]
+        if self._attributes["luck"] is None:
+            self._attributes["luck"] = self.dice_roll(1) + 6
+            self.initial_luck = self._attributes["luck"]
+
+    @property
+    def skill(self):
+        return self._attributes["skill"]
+
+    @skill.getter
+    def skill(self):
+        return self._attributes["skill"]
+
+    @skill.setter
+    def skill(self, value):
+        print(value, self.initial_skill)
+        switch = {
+            True: value,
+            value > self.initial_skill: self.initial_skill,
+            value < 0: 0,
+        }
+        print(switch)
+        self._attributes["skill"] = switch[True]
+
+    @property
+    def stamina(self):
+        return self._attributes["stamina"]
+
+    @stamina.getter
+    def stamina(self):
+        return self._attributes["stamina"]
+
+    @stamina.setter
+    def stamina(self, value):
+        switch = {
+            True: value,
+            value > self.initial_stamina: self.initial_stamina,
+            value < 0: 0,
+        }
+        self._attributes["stamina"] = switch[True]
+
+    @property
+    def luck(self):
+        return self._attributes["luck"]
+
+    @luck.getter
+    def luck(self):
+        return self._attributes["luck"]
+
+    @luck.setter
+    def luck(self, value):
+        switch = {
+            True: value,
+            value > self.initial_luck: self.initial_luck,
+            value < 0: 0,
+        }
+        self._attributes["luck"] = switch[True]
+
+    @property
+    def magic(self):
+        return self._attributes["magic"]
+
+    @magic.getter
+    def magic(self):
+        if self._attributes["magic"] is None:
+            self._attributes["magic"] = self.dice_roll(2) + 6
+        return self._attributes["magic"]
--- tcoc/tests/test_hero.py
+++ tcoc/tests/test_hero.py
@@ -23,3 +23,42 @@ class TestDice(unittest.TestCase):
         self.assertEqual(most_common,
                          7,
                          "7 is not the most commonresult for 2-dice rolls")
+
+
+class TestHero(unittest.TestCase):
+
+    def test_random_heros(self):
+        heroes = [hero.Hero() for _ in range(1000)]
+        self.assertTrue(all(7 <= x.skill <= 12 for x in heroes),
+                        "Not all heroes have normal skills")
+        self.assertTrue(all(14 <= x.stamina <= 24 for x in heroes),
+                        "Not all heroes have normal stamina")
+        self.assertTrue(all(7 <= x.luck <= 12 for x in heroes),
+                        "Not all heroes have normal luck")
+
+    def test_hero_skill(self):
+        test_hero = hero.Hero()
+        test_hero.skill += 100
+        self.assertTrue(7 <= test_hero.skill <= 12,
+                        "hero skill increased beyond initial value")
+        test_hero.skill -= 100
+        self.assertEqual(test_hero.skill, 0,
+                         "hero skill decreased beyond zero")
+
+    def test_hero_stamina(self):
+        test_hero = hero.Hero()
+        test_hero.stamina += 100
+        self.assertTrue(14 <= test_hero.stamina <= 24,
+                        "hero stamina increased beyond initial value")
+        test_hero.stamina -= 100
+        self.assertEqual(test_hero.stamina, 0,
+                         "hero skill decreased beyond zero")
+
+    def test_hero_luck(self):
+        test_hero = hero.Hero()
+        test_hero.luck += 100
+        self.assertTrue(7 <= test_hero.luck <= 12,
+                        "hero luck increased beyond initial value")
+        test_hero.luck -= 100
+        self.assertEqual(test_hero.luck, 0,
+                         "hero luck decreased beyond zero")
